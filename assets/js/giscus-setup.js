33f91ef639f85dfbe3ffa7c4408c389395370632
Seyviour <42647840+Seyviour@users.noreply.github.com>
2025-09-24 18:20:43 +0000

    Deploying to gh-pages from @ Seyviour/seyviour.github.io@b7479bd2c025c007062e8b59d2e2c4649127a602 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Seyviour/new_site",
+    "data-repo": "Seyviour/seyviour.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
